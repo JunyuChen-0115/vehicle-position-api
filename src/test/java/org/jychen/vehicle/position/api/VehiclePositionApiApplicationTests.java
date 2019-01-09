b3de42ee069f0ecbe9c99706ead0d937a3663a41
Junyu Chen <JunyuChen-0115@users.noreply.github.com>
2019-01-09 15:27:14 -0700

    Set up project

---
+++ src/test/java/org/jychen/vehicle/position/api/VehiclePositionApiApplicationTests.java
@@ -0,0 +1,17 @@
+package org.jychen.vehicle.position.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VehiclePositionApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
