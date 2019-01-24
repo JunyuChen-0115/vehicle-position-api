dd261331be190b96b5343654838e56021e224ed1
Junyu Chen <JunyuChen-0115@users.noreply.github.com>
2019-01-23 23:53:03 -0700

    Enable endpoint to get top record or find all records for a vehicle

--- vehicle-position-api-webservice/src/main/java/org/jychen/vehicle/position/api/controller/VehiclePositionController.java
+++ vehicle-position-api-webservice/src/main/java/org/jychen/vehicle/position/api/controller/VehiclePositionController.java
@@ -1,6 +1,8 @@
 package org.jychen.vehicle.position.api.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.jychen.vehicle.position.api.domain.VehiclePosition;
 import org.jychen.vehicle.position.api.integration.dto.VehiclePositionDTO;
 import org.jychen.vehicle.position.api.service.VehiclePositionService;
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping(VehiclePositionController.BASE_URL)
@@ -41,9 +46,20 @@ public class VehiclePositionController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public VehiclePositionDTO findByCriteria(@RequestParam String vehicleName, @RequestParam(defaultValue = "1") String limit) {
+    public List<VehiclePositionDTO> findByCriteria(@RequestParam String vehicleName, @RequestParam(required = false) String limit) {
         logger.info("Received request to find position records by criteria: vehicleName {}, limit {}", vehicleName, limit);
-        VehiclePosition vehiclePosition = vehiclePositionService.getTopByVehicleNameOrderByTsDesc(vehicleName);
-        return objectMapper.convertValue(vehiclePosition, VehiclePositionDTO.class);
+        List<VehiclePosition> vehiclePositionList;
+        if (StringUtils.isEmpty(limit)) {
+            vehiclePositionList = vehiclePositionService.findAllByVehicleNameOrderByTsDesc(vehicleName);
+        } else {
+            VehiclePosition vehiclePosition = vehiclePositionService.getTopByVehicleNameOrderByTsDesc(vehicleName);
+            vehiclePositionList = Collections.emptyList();
+            if (vehiclePosition != null) {
+                vehiclePositionList = Collections.singletonList(vehiclePosition);
+            }
+        }
+
+        logger.info("{} number of results are found.", vehiclePositionList.size());
+        return objectMapper.convertValue(vehiclePositionList, new TypeReference<List<VehiclePositionDTO>>(){});
     }
 }
--- vehicle-position-api-webservice/src/test/java/org.jychen.vehicle.position.api/controller/VehiclePositionControllerTest.java
+++ vehicle-position-api-webservice/src/test/java/org.jychen.vehicle.position.api/controller/VehiclePositionControllerTest.java
@@ -1,6 +1,8 @@
 package controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import mockit.Expectations;
 import mockit.Injectable;
 import mockit.Tested;
 import mockit.Verifications;
@@ -15,8 +17,12 @@ import org.jychen.vehicle.position.api.integration.dto.VehiclePositionDTO;
 import org.jychen.vehicle.position.api.service.VehiclePositionService;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -67,19 +73,90 @@ public class VehiclePositionControllerTest {
     }
 
     @Test
-    public void findByCriteria_When_OnlyFirstRecordIsNeeded_Expect_InvokeGetTopByVehicleNameOrderByTsDescInVehiclePositionService() throws Exception {
+    public void findByCriteria_When_GetTopRecord_Expect_InvokeGetTopByVehicleNameOrderByTsDescInVehiclePositionService() throws Exception {
+        String vehicleName = "speeding_truck";
+
+        new Expectations() {{
+            vehiclePositionService.getTopByVehicleNameOrderByTsDesc(vehicleName);
+            result = getMockVehiclePosition();
+        }};
+
+        MvcResult mvcResult = mockMvc.perform(
+                get(controllerUnderTest.BASE_URL)
+                .param("vehicleName", vehicleName)
+                .param("limit", "1"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        List<VehiclePositionDTO> actualResponse = mapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<List<VehiclePositionDTO>>(){});
+        Assert.assertEquals(1, actualResponse.size());
+    }
+
+    @Test
+    public void findByCriteria_When_GetTopRecordNotFound_Expect_InvokeGetTopByVehicleNameOrderByTsDescInVehiclePositionService() throws Exception {
+        String vehicleName = "speeding_truck";
+
+        new Expectations() {{
+            vehiclePositionService.getTopByVehicleNameOrderByTsDesc(vehicleName);
+            result = null;
+        }};
+
+        MvcResult mvcResult = mockMvc.perform(
+                get(controllerUnderTest.BASE_URL)
+                        .param("vehicleName", vehicleName)
+                        .param("limit", "1"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        List<VehiclePositionDTO> actualResponse = mapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<List<VehiclePositionDTO>>(){});
+        Assert.assertEquals(0, actualResponse.size());
+    }
+
+//    @Test
+//    public void findByCriteria_When_GetTop2Record_Expect_InvokeGetTopByVehicleNameOrderByTsDescInVehiclePositionService() throws Exception {
+//        String vehicleName = "speeding_truck";
+//        String limit = "2";
+//        List<VehiclePosition> vehiclePositionList = new ArrayList<>();
+//        vehiclePositionList.add(getMockVehiclePosition());
+//        vehiclePositionList.add(getMockVehiclePosition());
+//
+//        new Expectations() {{
+//            vehiclePositionService.findByVehicleNameOrderByTsDesc(vehicleName, limit);
+//            result = vehiclePositionList;
+//        }};
+//
+//        MvcResult mvcResult = mockMvc.perform(
+//                get(controllerUnderTest.BASE_URL)
+//                        .param("vehicleName", vehicleName)
+//                        .param("limit", limit))
+//                .andExpect(status().isOk())
+//                .andReturn();
+//
+//        List<VehiclePositionDTO> actualResponse = mapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<List<VehiclePositionDTO>>(){});
+//        Assert.assertEquals(2, actualResponse.size());
+//    }
+
+    @Test
+    public void findByCriteria_When_FindAllRecordsForGivenVehicle_Expect_InvokeFindAllByVehicleNameOrderByTsDescInVehiclePositionService() throws Exception {
         String vehicleName = "speeding_truck";
 
         mockMvc.perform(
                 get(controllerUnderTest.BASE_URL)
-                .param("vehicleName", vehicleName)
-                .param("limit", "1"))
+                        .param("vehicleName", vehicleName))
                 .andExpect(status().isOk());
 
         new Verifications() {{
             String actualVehicleName;
-            vehiclePositionService.getTopByVehicleNameOrderByTsDesc(actualVehicleName = withCapture());
+            vehiclePositionService.findAllByVehicleNameOrderByTsDesc(actualVehicleName = withCapture());
             Assert.assertEquals(vehicleName, actualVehicleName);
         }};
+    }
+
+    private VehiclePosition getMockVehiclePosition() {
+        VehiclePosition vehiclePosition = new VehiclePosition();
+        vehiclePosition.setId(UUID.randomUUID());
+        vehiclePosition.setVehicleName("speeding_truck");
+        vehiclePosition.setTs(new Date());
+        return vehiclePosition;
     }
 }
